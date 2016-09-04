60bb0ffb4c56fa39b01310f7a2a23090364bc3e0
Andrew Allen <a2intl@gmail.com>
2016-09-03 20:51:23 -0700

    add adc function

--- lisp.c
+++ lisp.c
@@ -833,6 +833,11 @@ PRIM dht(lisp pin) {
     return cons(mkint(t), mkint(h));
 }
 
+PRIM adc() {
+    int v = sdk_system_adc_read();
+    return mkint(v);
+}
+
 // CONTROL INTERRUPTS:
 // -------------------
 // (interrupt PIN 0)  : disable
@@ -1996,7 +2001,6 @@ PRIM printf_(lisp *envp, lisp all) {
             char type = *p++ = *f++;
             switch (type) {
             case '%':
-                putchar('%'); break;
             case 's':
                 princ(car(all)); break;
             case 'S':
@@ -3366,6 +3370,7 @@ lisp lisp_init() {
     DEFPRIM(in, 1, in);
     DEFPRIM(dht, 1, dht);
     DEFPRIM(interrupt, 2, interrupt);
+    DEFPRIM(adc, 0, adc);
 
     // system stuff
     DEFPRIM(gc, -1, gc);
